4d154eea25c5b30d6bcade81ade816411fa08523
ElionH <84781434+ElionH@users.noreply.github.com>
2023-02-15 11:44:51 +0100

    Presque terminé l'étape 8 mais il n'y a aucune donnée dans la bdd il semblerait.

--- app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
+++ app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
@@ -4,5 +4,5 @@ import android.app.Application
 import com.example.busschedule.database.AppDatabase
 
 class BusScheduleApplication : Application() {
-    val database: AppDatabase by lazy { AppDatabase.getDatabase(this)}
+    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
 }
