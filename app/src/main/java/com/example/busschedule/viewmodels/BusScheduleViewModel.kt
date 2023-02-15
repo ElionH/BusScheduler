73f497a351a4f85f0b593763a0c6ad10e0ed2204
ElionH <84781434+ElionH@users.noreply.github.com>
2023-02-15 11:02:51 +0100

    Fin Etape 7

--- app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -1,9 +1,9 @@
-package viewmodels
+package com.example.busschedule.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import database.schedule.Schedule
-import database.schedule.ScheduleDao
+import com.example.busschedule.database.schedule.Schedule
+import com.example.busschedule.database.schedule.ScheduleDao
 
 class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
     fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
