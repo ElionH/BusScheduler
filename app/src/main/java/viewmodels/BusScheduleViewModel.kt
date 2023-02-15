259eada5c0529a783ebbbd7cc271846134678ac0
ElionH <84781434+ElionH@users.noreply.github.com>
2023-02-15 10:22:11 +0100

    Fin Etape 6

---
+++ app/src/main/java/viewmodels/BusScheduleViewModel.kt
@@ -0,0 +1,24 @@
+package viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import database.schedule.Schedule
+import database.schedule.ScheduleDao
+
+class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
+    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
+
+    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+}
+
+class BusScheduleViewModelFactory(
+    private val scheduleDao: ScheduleDao
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return BusScheduleViewModel(scheduleDao) as T
+        }
+        throw java.lang.IllegalArgumentException("Unknown ViewModel class")
+    }
+}
